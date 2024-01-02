016bdd0275d1a5bd0b49b3f6a0c08c76ff8995d5
taku10101 <taku1011ht@gmail.com>
2024-01-02 19:30:45 +0900

    Validation Add

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private auth: AuthService) {}
 
   @Post('signup')
-  signUp(@Body('email') email: string, @Body('password') password: string) {
-    console.log(email, password);
+  signUp(@Body() dot: AuthDto) {
+    console.log(dot.password, dot.email);
     return this.auth.signup();
   }
 
   @Post('login')
-  login(@Body('email') email: string, @Body('password') password: string) {
-    console.log(email, password);
+  login() {
     return this.auth.login();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3005);
+  //validationのパイプをグローバルに設定
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //不要なパラメータを弾く
+    }),
+  );
+  await app.listen(3333);
 }
 bootstrap();
