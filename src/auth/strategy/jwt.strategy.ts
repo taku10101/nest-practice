4dd6e261136884877ebc7f3b299fa73f7e81aae0
taku10101 <taku1011ht@gmail.com>
2024-01-11 13:07:16 +0900

    return fix

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -2,12 +2,21 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(config: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), // リクエストヘッダーからJWTを取得する
-      ignoreExpiration: config.get('JWT_SECRET'), // JWTの有効期限を確認するかどうか
+      ignoreExpiration: false, // JWTの有効期限を確認するかどうか
+      secretOrKey: config.get<string>('JWT_SECRET'), // JWTの署名検証に使用する秘密鍵または公開鍵
     });
+  }
+
+  // JWTのペイロードを検証し、リクエストのユーザー情報を提供する
+  async validate(payload: any) {
+    // ここでユーザー情報を検証し、リクエストのユーザー情報を返す
+    // 例えば、以下のようにペイロードからユーザーIDを返すことができます：
+    return payload;
   }
 }
