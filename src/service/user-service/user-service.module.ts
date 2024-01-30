baf3f5b5c4c647e0c9826d0e636f1bfd6c228af3
taku10101 <taku1011ht@gmail.com>
2024-01-30 14:14:46 +0900

    user service add

--- src/service/user-service/user-service.module.ts
+++ src/service/user-service/user-service.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { UserServiceService } from './user-service.service';
+import { UserService } from './user-service.service';
+import { UserRepository } from 'src/repository/user.repository';
 
 @Module({
-  providers: [UserServiceService],
+  imports: [],
+  providers: [UserService, UserRepository],
 })
-export class UserServiceModule {
-  constructor(private userService: UserServiceService) {}
-}
+export class UserServiceModule {}
--- src/service/user-service/user-service.service.ts
+++ src/service/user-service/user-service.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto, UpdateUserDto } from 'src/dto/user';
+import { UserRepository } from 'src/repository/user.repository';
 
 @Injectable()
-export class UserServiceService {}
+export class UserService {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async createUser(data: CreateUserDto) {
+    return await this.userRepository.createUser(data);
+  }
+  async findAllUser() {
+    return await this.userRepository.findAll();
+  }
+  async findByUserID(id: number) {
+    return await this.userRepository.findByUserID(id);
+  }
+  async findByUserEmail(email: string) {
+    return await this.userRepository.findByUserEmail(email);
+  }
+  async updateUser(user_id: number, data: UpdateUserDto) {
+    return await this.userRepository.updateUser(user_id, data);
+  }
+  async deleteUser(user_id: number) {
+    return await this.userRepository.deleteUser(user_id);
+  }
+}
