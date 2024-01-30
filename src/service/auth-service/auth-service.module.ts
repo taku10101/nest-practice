eddd1e8042f6ed2343e796ee124a3bab91617e86
taku10101 <taku1011ht@gmail.com>
2024-01-30 14:43:32 +0900

    auth service add

--- src/service/auth-service/auth-service.module.ts
+++ src/service/auth-service/auth-service.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthServiceService } from './auth-service.service';
+import { UserRepository } from 'src/repository/user.repository';
 
 @Module({
-  providers: [AuthServiceService]
+  imports: [],
+  providers: [AuthServiceService, UserRepository],
 })
 export class AuthServiceModule {}
--- src/service/auth-service/auth-service.service.ts
+++ src/service/auth-service/auth-service.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { LoginDto, RegisterDto } from 'src/dto/auth';
+import { UserRepository } from 'src/repository/user.repository';
 
 @Injectable()
-export class AuthServiceService {}
+export class AuthServiceService {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async register(data: RegisterDto) {
+    return await this.userRepository.createUser(data);
+  }
+  async login(data: LoginDto) {
+    return await this.userRepository.findByUserEmail(data.email);
+  }
+}
